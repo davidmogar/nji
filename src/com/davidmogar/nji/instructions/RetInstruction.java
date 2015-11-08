68d022a4583e0b6766568ef4c48dec707b653100
David Moreno García <david.mogar@gmail.com>
2015-11-08 16:36:44 +0100

    Fix return instruction execute code
    I was not moving the value returned.

--- src/com/davidmogar/nji/instructions/RetInstruction.java
+++ src/com/davidmogar/nji/instructions/RetInstruction.java
@@ -16,11 +16,47 @@ public class RetInstruction implements Instruction {
 
     @Override
     public void execute(Context context) {
+        float value = readReturnedValue(context);
+
         context.stackPointer += returnOffset + localVariablesOffset + parametersOffset;
         context.basePointer = context.stack.getInteger(context.stackPointer);
         context.stackPointer += 2;
         context.instructionPointer = context.stack.getInteger(context.stackPointer);
-        context.stackPointer += 2;
+        context.stackPointer += 2 - returnOffset;
+
+        writeReturnedValue(value, context);
+    }
+
+    private float readReturnedValue(Context context) {
+        float value = 0;
+
+        switch (returnOffset) {
+            case 1:
+                value = context.stack.getByte(context.stackPointer);
+                break;
+            case 2:
+                value = context.stack.getInteger(context.stackPointer);
+                break;
+            case 3:
+                value = context.stack.getFloat(context.stackPointer);
+                break;
+        }
+
+        return value;
+    }
+
+    private void writeReturnedValue(float value, Context context) {
+        switch (returnOffset) {
+            case 1:
+                context.stack.setByte((byte) value, context.stackPointer);
+                break;
+            case 2:
+                context.stack.setInteger((int) value, context.stackPointer);
+                break;
+            case 3:
+                context.stack.setFloat(value, context.stackPointer);
+                break;
+        }
     }
 
 }
